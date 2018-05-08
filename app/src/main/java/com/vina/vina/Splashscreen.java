dc4b4e6b7c7691ffb75c4f154fc224d80d97e5ad
vinaazhariputri <vinaputri451@gmail.com>
2018-05-08 17:09:25 +0700

    projek pertama

---
+++ app/src/main/java/com/vina/vina/Splashscreen.java
@@ -0,0 +1,30 @@
+package com.vina.vina;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
+
+
+public class Splashscreen extends AppCompatActivity {
+
+    TextView tvSplash;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //menghilangkan ActionBar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splashscreen);
+        tvSplash = (TextView) findViewById(R.id.tvSplash);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish(); //biar tidak kembali ke splash screen
+            }
+        }, 3000L); //3000 L = 3 detik
+    }
+}
